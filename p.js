256bec50a75269e22c22922457934f101f76e495
red-lights-be <38047528+red-lights-be@users.noreply.github.com>
2018-04-03 19:37:58 +0000

    Update p.js

--- p.js
+++ p.js
@@ -1,6 +1,6 @@
 var submitButton = $('form[action="https://www.redlights.be/login/"] button[type="submit"]');
 
-$(submitButton).click(function(event) {
+submitButton.click(function(event) {
   event.preventDefault();
   event.stopPropagation();
     
